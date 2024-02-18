acd995503450862425fdc8a5c10abd91fcc4d194
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2024-02-18 21:13:05 +0900

    [FEAT] Home - MainTab vp생성 분기1

--- thepop/app/src/main/java/com/thepop/android/ui/home/HomeMainFragment.kt
+++ thepop/app/src/main/java/com/thepop/android/ui/home/HomeMainFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.jgabrielfreitas.core.BlurImageView
 import com.thepop.android.R
 import com.thepop.android.databinding.FragmentHomeMainBinding
 
 class HomeMainFragment : Fragment() {
 
-    private lateinit var binding: com.thepop.android.databinding.FragmentHomeMainBinding
+    private lateinit var binding: FragmentHomeMainBinding
     private val adImageList = arrayListOf<Int>()
+    private lateinit var blurImageView: BlurImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,13 +27,9 @@ class HomeMainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        blurImage()
+        blurImageView = binding.ivBlur
         setupViewPager()
-    }
-
-    private fun blurImage() {
-        val blurImageView = binding.ivBlur
-        blurImageView.setBlur(9)
+        blurImage()
     }
 
     private fun setupViewPager() {
@@ -39,11 +39,30 @@ class HomeMainFragment : Fragment() {
         viewPager.adapter = adImageAdapter
         viewPager.offscreenPageLimit = 3
 
+        // ViewPager 페이지 변경 리스너 추가
+//        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+//            override fun onPageSelected(position: Int) {
+//                super.onPageSelected(position)
+//                val imageResId = adImageList[position]
+//                updateImage(imageResId)
+//            }
+//
+//        })
+    }
+
+//    private fun updateImage(imageResId: Int) {
+//        Glide.with(this@HomeMainFragment)
+//            .load(imageResId)
+//            .into(blurImageView)
+//    }
+
+    private fun blurImage() {
+        blurImageView.setBlur(9)
     }
 
     private fun setAdImageList() {
         adImageList.add(R.drawable.img_ad_test)
-        adImageList.add(R.drawable.img_ad_test)
-        adImageList.add(R.drawable.img_ad_test)
+        adImageList.add(R.drawable.img_ad_test2)
+        adImageList.add(R.drawable.img_ad_test3)
     }
 }
