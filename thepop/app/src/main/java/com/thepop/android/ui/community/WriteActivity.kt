3fbe459dced266591136fe8921296f7c1b84a5b3
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2024-02-20 17:46:18 +0900

    [FEAT] community 기초 요소 세팅, post 리사이클러뷰 생성

---
+++ thepop/app/src/main/java/com/thepop/android/ui/community/WriteActivity.kt
@@ -0,0 +1,12 @@
+package com.thepop.android.ui.community
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thepop.android.R
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
