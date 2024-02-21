f5e89c9d4ca07897e2ced1038295ec55b63ea0f4
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2024-02-21 10:51:05 +0900

    [FEAT] 커뮤니티 글 쓰기 layout 마무리, main 페이지에 action bar 생성 및 write activity 연결

--- thepop/app/src/main/java/com/thepop/android/ui/community/CommunityMainFragment.kt
+++ thepop/app/src/main/java/com/thepop/android/ui/community/CommunityMainFragment.kt
@@ -1,13 +1,17 @@
 package com.thepop.android.ui.community
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.thepop.android.R
+import com.thepop.android.databinding.FragmentCommunityMainBinding
 
 class CommunityMainFragment : Fragment() {
+
+    private lateinit var binding: FragmentCommunityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +19,20 @@ class CommunityMainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community_main, container, false)
+        binding = FragmentCommunityMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        goWrite()
+    }
+
+    private fun goWrite() {
+        binding.fabCommunity.setOnClickListener {
+            val intent = Intent(context, WriteActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
